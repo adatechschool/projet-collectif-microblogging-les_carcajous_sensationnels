999cc51f8480ff302d47f9ec3f78806b7321bb17
Jean-Clément <jeanclement@audusseau.fr>
2024-04-10 16:52:39 +0200

    relation post user (hasMany)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class PostController extends Controller
 {
-    public function index(): Response
+    public function index(): View
     {
-        return response('Hello, World!');
-    } 
+        return view('post.index');
+    }
+    public function store(Request $request): RedirectResponse{
+        $validated = $request->validate([
+
+        'message' => 'required|string|max:255',
+
+    ]);
+
+
+
+    $request->user()->post()->create($validated);
+
+
+
+    return redirect(route('post.index'));} 
 }
