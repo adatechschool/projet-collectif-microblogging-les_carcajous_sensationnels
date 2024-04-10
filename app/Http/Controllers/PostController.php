3cbbd5a303492ad487488bc8dd3fde0624fe677f
PromiseHerb <146881805+PromiseHerb@users.noreply.github.com>
2024-04-10 15:26:05 +0200

    constroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PostController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        return response('Hello, World!');
+    } 
 }
