f770c3cab5817aaf764cd59ac70fe82d2cd102de
Fabienne-Lis <fabienne.lis@gmail.com>
2024-04-11 14:53:24 +0200

    change messages in content

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
     public function store(Request $request): RedirectResponse{
         $validated = $request->validate([
 
-        'message' => 'required|string|max:255',
+        'content' => 'required|string|max:255',//mettre content à message
 
     ]);
 
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -3,11 +3,11 @@
         <form method="POST" action="{{ route('post.store') }}">
             @csrf
             <textarea
-                name="message"
+                name="content"
                 placeholder="{{ __('What\'s on your mind?') }}"
                 class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-            >{{ old('message') }}</textarea>
-            <x-input-error :messages="$errors->get('message')" class="mt-2" />
+            >{{ old('content') }}</textarea>
+            <x-input-error :messages="$errors->get('content')" class="mt-2" />
             <x-primary-button class="mt-4">{{ __('Post') }}</x-primary-button>
         </form>
     </div>
